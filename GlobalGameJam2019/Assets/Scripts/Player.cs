f476dd9c103797a728a4678007f71384e50c813d
ITS Student <its-student@unl.edu>
2019-01-25 20:46:31 -0600

    Meaningless Player Update

--- GlobalGameJam2019/Assets/Scripts/Player.cs
+++ GlobalGameJam2019/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Wop");
+        Debug.Log("Flop");
     }
 
     // Update is called once per frame
