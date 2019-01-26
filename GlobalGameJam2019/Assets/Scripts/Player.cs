e3673a2be322962dfee6fbe5b02cc6f6d2ec5ec5
ITS Student <its-student@unl.edu>
2019-01-25 20:42:20 -0600

    Created empty player script, gameobject

---
+++ GlobalGameJam2019/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Wop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject.Transform.X += 1;
+    }
+}
