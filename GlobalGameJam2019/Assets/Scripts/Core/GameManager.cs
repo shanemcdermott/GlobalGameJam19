70fb85211788fc165040f460a5fffc7081caa875
Shane <shanemcdermott@unomaha.edu>
2019-01-26 15:50:20 -0600

    Added GameOver function, GameManager prefab/static instance.

--- GlobalGameJam2019/Assets/Scripts/Core/GameManager.cs
+++ GlobalGameJam2019/Assets/Scripts/Core/GameManager.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    public static GameManager instance = null;
+
     public Player player;
     public ObjectivesManager objectivesManager;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        if(player==null)
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        InitScene();
+    }
+
+    void InitScene()
+    {
+        if (player == null)
         {
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         }
-
+        if(objectivesManager == null)
+        {
+            objectivesManager = GetComponent<ObjectivesManager>();
+            objectivesManager.OnAllObjectivesComplete.AddListener(NotifyLevelComplete);
+            objectivesManager.OnAllObjectivesFailed.AddListener(GameOver);
+        }
     }
 
     // Update is called once per frame
@@ -27,5 +47,10 @@ public class GameManager : MonoBehaviour
     public void NotifyLevelComplete()
     {
         Debug.Log("All objectives are complete!");
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("All objectives are failed- Game Over!");
     }
 }
