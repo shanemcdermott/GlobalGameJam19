5f3f210b6a5c1b0557e7383a79d31d62012e8491
uri.herszbaumharding@gmail.com <uri.herszbaumharding@gmail.com>
2019-01-26 12:03:01 -0600

    Added Player sprite, basic movement, house

--- GlobalGameJam2019/Assets/Scripts/Player.cs
+++ GlobalGameJam2019/Assets/Scripts/Player.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int i = 0;
+    public Vector2 move;
+    public float speed = 1;
+    //public int i = 0;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Woop");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject.Transform.X += 1;
+        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow))
+        {
+            //Debug.Log("UP");
+            move.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            //Debug.Log("DOWN");
+            move.y = -1;
+        }
+        else
+        {
+            //Debug.Log("Stahp");
+            move.y = 0;
+        }
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Debug.Log("LEFT");
+            move.x = -1;
+        }     
+       else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            //Debug.Log("RIGHT");
+            move.x = 1;
+        }
+        else
+        {
+            //Debug.Log("Stahp");
+            move.x = 0;
+        }
+
+        transform.Translate(move * speed);
     }
+
+    
 }
